b9bce9a632f90ff3fc3715bc2e81fcff17b1072b
Julius Kolosnjaji <juliuskolosnjaji0404@gmail.com>
2021-05-17 19:41:54 +0200

    test2

--- MiningTurtle/main.lua
+++ MiningTurtle/main.lua
@@ -1,12 +1,5 @@
 turtle.refuel()
-print("Refueled turtle!")
-while turtle.detectDown() do
-    print("Digging down!")
-    turtle.digDown()
-    print("Going down!")
-    turtle.down()
-    print("Digging forward!")
-    turtle.dig()
-    print("Going forward!")
+while true do
     turtle.forward()
+    turtle.dig()
 end
