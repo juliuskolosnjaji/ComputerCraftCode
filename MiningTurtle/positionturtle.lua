8ea3c4ab40c6d48438956979ae35689dda0740b8
Julius Kolosnjaji <juliuskolosnjaji0404@gmail.com>
2021-05-19 21:27:20 +0200

    t +

--- MiningTurtle/positionturtle.lua
+++ MiningTurtle/positionturtle.lua
@@ -7,20 +7,15 @@ function moveto()
     turtle.forward()
     local abstandz = turtlez-Targetz
     local currentx, currenty, currentz = gps.locate()
-
+    
+    if currentz == abstandz then
+        turtle.turnLeft()
+        moveto()
     if currentz > abstandz then
         turtle.turnLeft()
         turtle.turnLeft()
     end
-    if currentz == abstandz then
-        turtle.turnLeft()
-        moveto()
-    end
-    for i = 1, Targetz 
-    do
-        turtle.forward()
-        if turtle.detect() == true then
-            turtle.dig()
-        end
     end
 end
+
+moveto()
